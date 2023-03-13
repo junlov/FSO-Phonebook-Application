c5ab59e90be83fc77662f426a1556d4eb1e7a336
Junior Love <107013570+junlov@users.noreply.github.com>
2023-03-13 13:51:25 -0400

    Update env.variable for PORT

--- server/index.js
+++ server/index.js
@@ -4,8 +4,8 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use(morgan("tiny"));
 
 morgan.token("postLog", (req, res) => {
@@ -110,5 +110,5 @@ app.post("/api/persons/", (req, res) => {
   }
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, (req, res) => console.log(`Server listening on port ${PORT}`));
