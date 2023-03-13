6b401b8a8b69d1d8e5658b37bb6a69dd6aca698a
Junior Love <107013570+junlov@users.noreply.github.com>
2023-03-13 16:01:13 -0400

    Moving front-end to Render

--- server/index.js
+++ server/index.js
@@ -4,9 +4,9 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
+app.use(cors());
 
 morgan.token("postLog", (req, res) => {
   return req;
